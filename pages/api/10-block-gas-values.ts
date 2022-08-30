4a698cfe76463131aa63ed4e294cf451e3597be6
Mark King <markpking2@gmail.com>
2022-08-30 16:54:37 -0400

    api endpoints working

---
+++ pages/api/10-block-gas-values.ts
@@ -0,0 +1,52 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { fromHex } from "alchemy-sdk";
+import { ethers } from "ethers";
+const alchemySdk = require("api")("@alchemy-docs/v1.0#5p1de4kl6covpyp");
+import * as _ from "lodash/fp";
+
+const get10LatestBaseFees = async function () {
+  const history = await alchemySdk.ethFeeHistory(
+    {
+      id: 1,
+      jsonrpc: "2.0",
+      method: "eth_feeHistory",
+      params: [10, "latest"],
+    },
+    { apiKey: process.env.ALCHEMY_API_URL }
+  );
+  const baseFees = _.map(
+    (fee: string) => ethers.utils.formatUnits(fromHex(fee), "ether"),
+    _.slice(0, 10, _.get("result.baseFeePerGas", history))
+  );
+  const oldestBlock = _.get("result.oldestBlock", history);
+  const oldestBlockDecimal = parseInt(oldestBlock, 16);
+  const blockNumbers = _.map(
+    (key) => parseInt(key, 10) + oldestBlockDecimal,
+    _.keys(Array(10))
+  );
+
+  return {
+    baseFees: _.zip(blockNumbers, baseFees),
+    gasUsedRatios: _.zip(blockNumbers, _.get("result.gasUsedRatio", history)),
+  };
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case "GET": {
+      try {
+        const baseFeesByBlockNumber = await get10LatestBaseFees();
+        res.status(200).json(baseFeesByBlockNumber);
+      } catch (err) {
+        res.status(500).json({ message: "Error getting block gas values." });
+      }
+      break;
+    }
+    default:
+      res.setHeader("Allow", ["POST"]);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
---
+++ pages/api/10-block-transfer-counts.ts
@@ -0,0 +1,83 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Alchemy } from "alchemy-sdk";
+const alchemySdk = require("api")("@alchemy-docs/v1.0#4y8wt5c9l79g4ign");
+import * as _ from "lodash/fp";
+import { toHex } from "alchemy-sdk";
+
+import { alchemySettings } from "../../config";
+
+const alchemy = new Alchemy(alchemySettings);
+
+const getTransferCountsByBlock = async function (
+  fromBlock: string,
+  toBlock: string,
+  address: string
+) {
+  const transfers = [];
+  let keepFetching = true;
+  let lastPageKey;
+  while (keepFetching) {
+    // @ts-ignore
+    const result = await alchemySdk.alchemyGetAssetTransfers(
+      {
+        id: 1,
+        jsonrpc: "2.0",
+        method: "alchemy_getAssetTransfers",
+        params: [
+          {
+            fromBlock,
+            toBlock,
+            withMetadata: false,
+            excludeZeroValue: true,
+            maxCount: "0x3e8",
+            category: ["erc20"],
+            contractAddress: [address],
+            pageKey: lastPageKey || undefined,
+          },
+        ],
+      },
+      {
+        apiKey: process.env.ALCHEMY_API_KEY,
+      }
+    );
+    transfers.push(_.get("result.transfers", result));
+    lastPageKey = _.get("result.pageKey", result);
+    if (!lastPageKey) {
+      keepFetching = false;
+    }
+  }
+  return _.mapValues(
+    (transactions: Array<Record<string, unknown>>) => ({
+      count: _.size(transactions),
+    }),
+    _.groupBy("blockNum", _.flattenDeep(transfers))
+  );
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case "GET": {
+      try {
+        const { address } = req.query;
+        const toBlock = await alchemy.core.getBlockNumber();
+        const fromBlock = toBlock - 9;
+        const transferCounts = await getTransferCountsByBlock(
+          toHex(fromBlock),
+          toHex(toBlock),
+          address as string
+        );
+
+        res.status(200).json(transferCounts);
+      } catch (err) {
+        res.status(405).json({ message: "Error getting block transfers" });
+      }
+      break;
+    }
+    default:
+      res.setHeader("Allow", ["POST"]);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
